07c04e0f27070d58174e0f4bc3746db04f3b1c68
grasielaGomes <produtoradama@gmail.com>
2025-12-18 21:55:12 +0100

    feat: define GraphQL type definitions for categories with CRUD operations

---
+++ backend/src/modules/categories/categories.typeDefs.ts
@@ -0,0 +1,25 @@
+export const categoriesTypeDefs = /* GraphQL */ `
+  type Category {
+    id: ID!
+    name: String!
+  }
+
+  input CreateCategoryInput {
+    name: String!
+  }
+
+  input UpdateCategoryInput {
+    id: ID!
+    name: String!
+  }
+
+  extend type Query {
+    categories: [Category!]!
+  }
+
+  extend type Mutation {
+    createCategory(input: CreateCategoryInput!): Category!
+    updateCategory(input: UpdateCategoryInput!): Category!
+    deleteCategory(id: ID!): Boolean!
+  }
+`
