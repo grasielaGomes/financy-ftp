915a28e596398f0003123de4151a84a0d4ff95dc
grasielaGomes <produtoradama@gmail.com>
2026-01-05 21:59:50 +0100

    chore: add main entry point for frontend application

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from '@/routes/router'
+import { GraphQLProvider } from '@/lib/graphql/client'
+import '@/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GraphQLProvider>
+      <RouterProvider router={router} />
+    </GraphQLProvider>
+  </React.StrictMode>
+)
