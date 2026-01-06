9377f185fb68de70b49a62132d46333087347133
grasielaGomes <produtoradama@gmail.com>
2026-01-06 21:31:52 +0100

    feat: add Logo component to display the Financy logo

---
+++ frontend/src/components/ui/Logo.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+
+import logo from "@/assets/logo.png"
+import { cn } from "@/lib/utils"
+
+const Logo = React.forwardRef<
+  HTMLImageElement,
+  React.ImgHTMLAttributes<HTMLImageElement>
+>(({ className, alt = "Financy", ...props }, ref) => {
+  return (
+    <img
+      ref={ref}
+      src={logo}
+      alt={alt}
+      width={134}
+      height={32}
+      className={cn("h-8 w-[134px]", className)}
+      {...props}
+    />
+  )
+})
+Logo.displayName = "Logo"
+
+export { Logo }
