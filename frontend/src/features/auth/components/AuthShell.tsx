19bd2401b3b6af28e03c6caa0e621adb5d1363e8
grasielaGomes <produtoradama@gmail.com>
2026-01-12 20:03:28 +0100

    refactor: simplify AuthShell component structure and enhance layout with FormLayout

--- frontend/src/features/auth/components/AuthShell.tsx
+++ frontend/src/features/auth/components/AuthShell.tsx
@@ -1,6 +1,5 @@
-import { Card } from '@/components/ui/Card'
-import { Logo } from '@/components/ui/Logo'
 import type { ReactNode } from 'react'
+import { FormHeader, FormLayout } from '@/components/layout/FormLayout'
 
 type AuthShellProps = {
   title: string
@@ -10,17 +9,10 @@ type AuthShellProps = {
 
 export const AuthShell = ({ title, subtitle, children }: AuthShellProps) => {
   return (
-    <div className="min-h-dvh w-full bg-background">
-      <div className="mx-auto flex min-h-dvh w-full max-w-[448px] flex-col justify-center py-10 gap-8 items-center">
-        <Logo />
-        <Card className="p-8 w-full ">
-          <header className="flex flex-col items-center mb-8">
-            <h1 className="text-xl font-bold text-gray-800">{title}</h1>
-            <p className="text-md text-gray-600">{subtitle}</p>
-          </header>
-          <>{children}</>
-        </Card>
-      </div>
+    <div className="py-10 md:py-12">
+      <FormLayout header={<FormHeader title={title} subtitle={subtitle} />}>
+        {children}
+      </FormLayout>
     </div>
   )
 }
