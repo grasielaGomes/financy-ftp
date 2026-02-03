8f2811dbca75ee09cb60594e88a9be285bfa9d26
grasielaGomes <produtoradama@gmail.com>
2026-02-03 18:02:13 -0300

    feat: add GraphQL queries and mutations for transaction management

---
+++ frontend/src/features/transactions/api/transactions.gql.ts
@@ -0,0 +1,64 @@
+import { gql } from '@apollo/client'
+
+export const TRANSACTIONS_QUERY = gql`
+  query Transactions($input: TransactionsQueryInput) {
+    transactions(input: $input) {
+      total
+      items {
+        id
+        title
+        amount
+        type
+        occurredAt
+        category {
+          id
+          name
+          iconKey
+          colorKey
+        }
+      }
+    }
+  }
+`
+
+export const CREATE_TRANSACTION_MUTATION = gql`
+  mutation CreateTransaction($input: CreateTransactionInput!) {
+    createTransaction(input: $input) {
+      id
+      title
+      amount
+      type
+      occurredAt
+      category {
+        id
+        name
+        iconKey
+        colorKey
+      }
+    }
+  }
+`
+
+export const UPDATE_TRANSACTION_MUTATION = gql`
+  mutation UpdateTransaction($input: UpdateTransactionInput!) {
+    updateTransaction(input: $input) {
+      id
+      title
+      amount
+      type
+      occurredAt
+      category {
+        id
+        name
+        iconKey
+        colorKey
+      }
+    }
+  }
+`
+
+export const DELETE_TRANSACTION_MUTATION = gql`
+  mutation DeleteTransaction($id: ID!) {
+    deleteTransaction(id: $id)
+  }
+`
