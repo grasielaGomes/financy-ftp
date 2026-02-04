99b167b29eaadd2c66f549ffe1d93b69a70593c6
grasielaGomes <produtoradama@gmail.com>
2026-02-04 09:48:29 -0300

    feat: remove unused Page5 and Page6 components

--- frontend/src/pages/Page5.tsx
+++
@@ -1,8 +0,0 @@
-export const Page5 = () => {
-  return (
-    <main className="page">
-      <h1>Page 5</h1>
-      <p>Placeholder até mapearmos o Figma.</p>
-    </main>
-  )
-}
--- frontend/src/pages/Page6.tsx
+++
@@ -1,8 +0,0 @@
-export const Page6 = () => {
-  return (
-    <main className="page">
-      <h1>Page 6</h1>
-      <p>Placeholder até mapearmos o Figma.</p>
-    </main>
-  )
-}
