9a68d413a1e69975ed671918f030663b47339668
grasielaGomes <produtoradama@gmail.com>
2026-02-04 07:49:33 -0300

    feat: add hooks index file to export transaction-related hooks and types

---
+++ frontend/src/features/transactions/hooks/index.ts
@@ -0,0 +1,8 @@
+export { useDebouncedValue } from './useDebouncedValue'
+export { useTransactionsPage } from './useTransactionsPage'
+
+export type {
+  TransactionDialogPayload,
+  TransactionRow,
+  TransactionsFilters,
+} from './useTransactionsPage'
